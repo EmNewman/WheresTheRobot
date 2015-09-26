478eff5b230ead69d9256d67ec4dc044221e9aed
bpsoderna <bpsoderna@gmail.com>
2015-09-26 10:05:12 -0400

    should return the address from the spreadsheet???

---
+++ js/readAddress.js
@@ -0,0 +1,24 @@
+<div class="results"></div> 
+ 
+<script>
+ 
+ // ID of the Google Spreadsheet
+ var spreadsheetID = "13oMpK1vV_7kHJuL0Md0ja3TUsqHh5L7VPl9zTmuZfxI";
+ 
+ // Make sure it is public or set to Anyone with link can view 
+ var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+ 
+ $.getJSON(url, function(data) {
+ 
+  var entry = data.feed.entry;
+ 
+  $(entry).each(function(){
+    // Column names are name, age, etc.
+    $('.results').prepend('<p>'+this.gsx$address.$t+this.gsx$city.$t+'</p>');
+  });
+ 
+	return results;
+
+ });
+ 
+</script>
