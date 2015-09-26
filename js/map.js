d64e40904989da73af55fcfbe426f7693a0f34df
xisun5 <xis1598@gmail.com>
2015-09-25 23:05:15 -0400

    hgfdfghj

--- js/map.js
+++ js/map.js
@@ -1,8 +1,27 @@
+<<<<<<< Updated upstream
 //map!!!!
 function initialize() 
 {
 	var mapCanvas = document.getElementById('map');
 	var pittLatLng = new google.maps.LatLng(40.44, -79.98);
+=======
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			#map {
+				width: 400px;
+				height: 500px;
+				float: left;
+			}
+		</style>
+		<script src="https://maps.googleapis.com/maps/api/js"></script>
+		<script>
+			function initialize() 
+			{
+				var mapCanvas = document.getElementById('map');
+				var pittLatLng = new google.maps.LatLng(40.44, -79.98);
+>>>>>>> Stashed changes
 
 	var mapOptions = {
 		center: pittLatLng,
@@ -18,4 +37,16 @@ function initialize()
 	var marker = new google.maps.Marker(markerOptions);
 }
 
+<<<<<<< Updated upstream
 google.maps.event.addDomListener(window, 'load', initialize);
+=======
+			google.maps.event.addDomListener(window, 'load', initialize);
+
+		</script>
+	</head>
+ 
+	<body>
+		<div id="map"></div>
+	</body>
+</html>
+>>>>>>> Stashed changes
