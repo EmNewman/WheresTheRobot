ef4fd66e2c65c4405f13712567bbf6cdc773b5c6
bpsoderna <bpsoderna@gmail.com>
2015-09-25 22:41:05 -0400

    added html as comment

--- js/map.js
+++ js/map.js
@@ -1,5 +1,16 @@
-//src="https://maps.googleapis.com/maps/api/js"
-function initialize() 
+/*<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			#map {
+				width: 400px;
+				height: 500px;
+				float: left;
+			}
+		</style>
+		<script src="https://maps.googleapis.com/maps/api/js"></script>
+		<script>*/
+			function initialize() 
 			{
 				var mapCanvas = document.getElementById('map');
 				var pittLatLng = new google.maps.LatLng(40.44, -79.98);
@@ -16,6 +27,22 @@ function initialize()
 					map: map }
 
 				var marker = new google.maps.Marker(markerOptions);
+
+				var infoWindowOptions = {
+					content: 'this is pittsburgh'}
+
+				var infowindow = new google.maps.InfoWindow(infoWindowOptions);
+
+				marker.addListener('click' function() {
+					infowindow.open(map, marker); });
 			}
 
 			google.maps.event.addDomListener(window, 'load', initialize);
+
+/*		</script>
+	</head>
+ 
+	<body>
+		<div id="map"></div>
+	</body>
+</html>*/
