9dc2ed2e937ec75a360b9dcf3e71090c7dc980f4
bpsoderna <bpsoderna@gmail.com>
2015-09-25 22:38:59 -0400

    how to map

---
+++ js/map.js
@@ -0,0 +1,21 @@
+//src="https://maps.googleapis.com/maps/api/js"
+function initialize() 
+			{
+				var mapCanvas = document.getElementById('map');
+				var pittLatLng = new google.maps.LatLng(40.44, -79.98);
+
+				var mapOptions = {
+					center: pittLatLng,
+					zoom: 6,
+					mapTypeId: google.maps.MapTypeId.ROADMAP }
+
+				var map = new google.maps.Map(mapCanvas, mapOptions);
+
+				var markerOptions = {
+					position: pittLatLng,
+					map: map }
+
+				var marker = new google.maps.Marker(markerOptions);
+			}
+
+			google.maps.event.addDomListener(window, 'load', initialize);
